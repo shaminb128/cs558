f3e9e6233d770008cea4ee85af71424a066359c3
Harry <zhan849@usc.edu>
2015-10-07 16:01:44 -0700

    added methods to routing.h

--- lab9/packet_util.h
+++ lab9/packet_util.h
@@ -7,6 +7,11 @@
  #ifndef _PACKET_UTIL_H_
  #define _PACKET_UTIL_H_
  #include "packet.h"
- 
+
+/* checksum generation */
+ u_int16_t rthdr_chk_gen(struct rthdr*);
+ u_int16_t chdr_chk_gen(u_char* packet, int size);
+ u_int16_t urhdr_chk_gen(u_char* packet, int size);
+ u_int16_t rlhdr_chk_gen(u_char* packet, int size);
 
  #endif
--- lab9/routing.h
+++ lab9/routing.h
@@ -1,3 +1,6 @@
+#ifndef _ROUTING_H_
+#define _ROUTING_H_
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
@@ -22,9 +25,33 @@
 
 extern uint64_t routing_table[20];
 
+typedef enum {
+	// Routing options
+	P_DO_NOTHING,			/* this interface sent the packet, do nothing */
+	P_FORWARD,				/* Forward the packet to next hop */
+	P_TIMEOUT,				/* Drop the packet and generate ICMP timeout reply */
+	P_ERRCHK,				/* Checksum does not match the packet */
+	P_NOT_YET_IMPLEMENTED	/* Used only for not-yet-implemented functions */
+} r_op;
+
+typedef enum {
+	// Packet forward type
+	P_LOCAL,
+	P_REMOTE
+} pf_t;
+
 int rt_tbl_size;
 
 
 void createRT();
 void printRT(uint64_t *);
 
+/* takes in packet, return r_op */
+int routing_opt(u_char* packet);
+
+/* takes in packet, return routing table entry */
+uint64_t rt_lookup(u_char* packet);
+
+/* takes in packet, modify it for forwarding */
+int modify_packet(u_char* packet);
+#endif
