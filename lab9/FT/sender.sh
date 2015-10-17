530faa1f10a2a7a246d6cceb4e58b5a462e63e3b
Harry <zhan849@usc.edu>
2015-10-17 11:30:53 -0700

    modified sender script

--- lab9/FT/sender.sh
+++ lab9/FT/sender.sh
@@ -3,8 +3,8 @@ echo "===>start timestamp:"
 A=$(date +%s%N)
 echo $A
 sudo ./sender data1000.bin node4 43
-echo "===>end timestamp:"
 B=$(date +%s%N)
+echo "===>end timestamp:"
 echo $B
 echo "===>Throughput"
 echo "1048576000 / ($B - $A) * 1000000000 * 8" | bc -l
