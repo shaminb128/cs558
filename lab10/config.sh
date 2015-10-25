8ade95c840bc5bc2867e11e71d81f205a3bbcacd
vidhi goel <goel.vidhi07@gmail.com>
2015-10-24 22:49:48 -0700

    added config.sh script

---
+++ lab10/config.sh
@@ -0,0 +1,7 @@
+#!/bin/sh
+touch /etc/ovs-vswitchd.conf
+ovsdb-tool create /etc/openvswitch/conf.db vswitchd/vswitch.ovsschema
+ovsdb-server --remote=punix:/var/run/openvswitch/db.sock --remote=db:Open_vSwitch,Open_vSwitch,manager_options --private-key=db:Open_vSwitch,SSL,private_key --certificate=db:Open_vSwitch,SSL,certificate --bootstrap-ca-cert=db:Open_vSwitch,SSL,ca_cert --pidfile --detach
+ovs-vsctl --no-wait init
+ovs-vswitchd --pidfile --detach
+
